e52e002bbba1a3c6df1b830f85381ed3a6dd347b
Ivosin1 <ivshonowo@gmail.com>
2023-09-19 23:01:11 +0100

    Betty corrections

--- getters.c
+++ getters.c
@@ -104,13 +104,14 @@ int _getsize(const char *format, int *i)
 }
 
 /**
- * get_width - Calculates the width for printing
+ * _getwidth - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
  * Return: width.
  */
+
 int _getwidth(const char *format, int *i, va_list list)
 {
 	int current_index;
